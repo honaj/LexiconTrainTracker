b50e688e07761ae0a36ca151e4a8422f93d81c9e
honaj <honajnahoj@gmail.com>
2023-10-05 12:56:54 +0200

    Real init

--- Scenes/Route/Route.cs
+++ Scenes/Route/Route.cs
@@ -3,9 +3,8 @@ using System;
 
 public class Route : Node2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	[Export] Path2D routePath;
+	[Export] Station[] stations;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
